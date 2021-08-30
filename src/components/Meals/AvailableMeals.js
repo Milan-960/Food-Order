55d74c7ffe81f53c9c28ed95159fca699114c29e
Milan Sachani <milan.sachani.ms@gmail.com>
2021-08-30 15:24:00 +0200

    http request added

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,40 +1,63 @@
+import { useEffect, useState } from "react";
 import Card from "../UI/Card";
 import MealItem from "./MealItem/MealItem";
 import classes from "./AvailableMeals.module.css";
-
-const DUMMY_MEALS = [
-  {
-    id: "m1",
-    name: "Dhosa",
-    description:
-      "South Indian paper thin pancakes made from lentils and fermented rice flour, fried on a flat baking tray with ghee.",
-    price: 21,
-  },
-  {
-    id: "m2",
-    name: "Vada pav",
-    description:
-      "Vegetarian fast food dish originating in the state of Maharashtra.",
-    price: 15,
-  },
-  {
-    id: "m3",
-    name: "Manchurian",
-    description:
-      "Manchurian is a class of Indian Chinese dishes prepared by coarse chopping and deep-frying a main ingredient such as chicken, cauliflower, shrimp, fish, mutton or paneer cheese, and then frying in a sauce with the addition of soy sauce",
-    price: 40,
-  },
-  {
-    id: "m4",
-    name: "Paneer Tikka Masala",
-    description:
-      "Paneer tikka masala is an Indian dish of marinated paneer cheese served in a spice sauce",
-    price: 50,
-  },
-];
+import mealsImage from "../../Assets/404.jpg";
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => (
+  const [meal, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState(null);
+
+  useEffect(() => {
+    const fetchMeals = async () => {
+      const response = await fetch(
+        "https://food-app-123-default-rtdb.firebaseio.com/meals.json"
+      );
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
+
+      const responseData = await response.json();
+
+      const loadedMeals = [];
+
+      for (const key in responseData) {
+        loadedMeals.push({
+          id: key,
+          name: responseData[key].name,
+          description: responseData[key].description,
+          price: responseData[key].price,
+        });
+      }
+      setMeals(loadedMeals);
+      setIsLoading(false);
+    };
+
+    fetchMeals().catch((error) => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
+  }, []);
+
+  if (isLoading) {
+    return (
+      <section className={classes.mealsLoading}>
+        <p>Loading...</p>
+      </section>
+    );
+  }
+
+  if (httpError) {
+    return (
+      <section className={classes.mealserror}>
+        <p> Something went wrong!</p>
+        <img className={classes.errorimg} src={mealsImage} />
+      </section>
+    );
+  }
+
+  const mealsList = meal.map((meal) => (
     <MealItem
       key={meal.id}
       id={meal.id}
